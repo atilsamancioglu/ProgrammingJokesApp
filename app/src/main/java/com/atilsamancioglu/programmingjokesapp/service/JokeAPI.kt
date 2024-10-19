a4e129b91804d69fdc6056c7d8beee166e558953
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-19 10:20:36 +0300

    service added

--- app/src/main/java/com/atilsamancioglu/programmingjokesapp/service/JokeAPI.kt
+++ app/src/main/java/com/atilsamancioglu/programmingjokesapp/service/JokeAPI.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 interface JokeAPI {
 
     @GET("atilsamancioglu/ProgrammingJokesJSON/refs/heads/main/jokes.json")
-    fun getData(): List<Joke>
+    suspend fun getData(): List<Joke>
 
 }
